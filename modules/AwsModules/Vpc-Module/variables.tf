bcbb4bb9b24388438719a42794feb4e9a0cc368f
NAGIREDDYNANDYALA <118894739+NAGIREDDYNANDYALA@users.noreply.github.com>
2024-12-13 09:28:06 +0000

    modules-adding

---
+++ modules/AwsModules/Vpc-Module/variables.tf
@@ -0,0 +1,18 @@
+# Environment Variables
+variable "region" {}
+variable "project_name" {}
+variable "environment" {}
+
+#VPC variables
+variable "vpc_cidr" {}
+
+# Public Subnet variables
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+
+# Private-subent-variables
+variable "private_app_subnet_az1_cidr" {}
+variable "private_app_subnet_az2_cidr" {}
+variable "private_data_subnet_az1_cidr" {}
+variable "private_data_subnet_az2_cidr" {}
+#variable "" {}
